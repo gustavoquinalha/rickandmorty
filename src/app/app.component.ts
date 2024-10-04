b37f079dbe7481d18ee0e155a3869470c80f77c6
Gustavo Quinalha <gustavo_quinalha@hotmail.com>
2024-10-04 19:12:32 -0300

    Add ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { NavigationMenuComponent } from "./components/navigation-menu/navigation-menu.component";
 import { NavigationFooterComponent } from './components/navigation-footer/navigation-footer.component';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,14 @@ import { NavigationFooterComponent } from './components/navigation-footer/naviga
 })
 export class AppComponent {
   title = 'Rick and Morty';
+
+  constructor(private router: Router, private viewportScroller: ViewportScroller) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.viewportScroller.scrollToPosition([0, 0]);
+      }
+    });
+  }
 }
