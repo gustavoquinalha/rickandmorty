54bf8ddad442bb9b2d2f65353124f0c03b9442c9
Gustavo Quinalha <gustavo_quinalha@hotmail.com>
2024-10-07 02:04:37 -0300

    Add ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { Component, Renderer2 } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { NavigationMenuComponent } from "./components/navigation-menu/navigation-menu.component";
 import { NavigationFooterComponent } from './components/navigation-footer/navigation-footer.component';
 import { ViewportScroller } from '@angular/common';
@@ -16,10 +16,17 @@ export class AppComponent {
   title = 'Rick and Morty';
   private routeSubscription: Subscription = new Subscription();
 
-  constructor(private router: Router, private viewportScroller: ViewportScroller) { }
+  constructor(private router: Router, private viewportScroller: ViewportScroller, private renderer: Renderer2) { }
 
   ngOnInit(): void {
     this.routeSubscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.renderer.removeClass(document.body, 'pb-16');
+        if (event.url === '/' || event.url.includes('/episode/') || event.url.includes('/location/') || event.url.includes('/character/')) {
+          this.renderer.addClass(document.body, 'pb-16');
+        }
+      }
+
       if (event instanceof NavigationEnd) {
         this.viewportScroller.scrollToPosition([0, 0]);
       }
