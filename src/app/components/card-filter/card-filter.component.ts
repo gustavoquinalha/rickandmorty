898134fe2107836962b9884aa7b318bc596ccb98
Gustavo Quinalha <gustavo_quinalha@hotmail.com>
2024-10-08 20:48:21 -0300

    Add translate

--- src/app/components/card-filter/card-filter.component.ts
+++ src/app/components/card-filter/card-filter.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TooltipDirective } from '../../directives/tooltip.directive';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-card-filter',
   standalone: true,
-  imports: [CommonModule, TooltipDirective],
+  imports: [CommonModule, TooltipDirective, TranslateModule],
   templateUrl: './card-filter.component.html',
   styleUrl: './card-filter.component.scss'
 })
@@ -13,6 +14,7 @@ export class CardFilterComponent {
   @Input() title?: string;
   @Input() icon?: string;
   @Input() selected?: string;
+  @Input() key?: string;
   @Output() selectedEvent = new EventEmitter<string>();
 
   select(value: string) {
